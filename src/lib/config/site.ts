ded5854592b66860f2c6dafb8385efc764903d8a
Parth Rajkondawar <115873426+AkaniX3@users.noreply.github.com>
2024-06-11 17:02:46 +0530

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,8 +3,8 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
+  title: 'AkaniX3',
+  subtitle: 'Blog Page',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
