58465c48a87d5039aafe0ac418206e8b76e0e8f7
Parth Rajkondawar <115873426+AkaniX3@users.noreply.github.com>
2024-06-11 17:10:47 +0530

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -11,7 +11,7 @@ export const site: SiteConfig = {
     avatar: '/assets/maskable@512.png',
     name: 'AkaniX3',
     status: '✨',
-    bio: 'Welcome to my page<3\nHere i write about things i do!'
+    bio: `Welcome to my page<3<br/>Here i write about things i do!`
   },
   themeColor: '#3D4451'
 }
